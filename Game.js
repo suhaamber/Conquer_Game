b229ef94bccfe28d1cf0b2f72722af77bf2d4d8a
Suha Amber <suhaamber@gmail.com>
2021-03-17 00:04:45 +0400

    Minimax algorithm prerequisites complete.

--- Game.js
+++ Game.js
@@ -1,9 +1,12 @@
 var game_content = [0,0,0,0,0,0,0,0,0,0];
 const LINES = 10;
+const DEPTH = 2; 
+const ALPHA = -Infinity; 
+const BETA = Infinity; 
 var start = false; 
-var player_rooms=0, computer_rooms=0;
-
-
+const COMPUTER_TURN = 1;
+const PLAYER_TURN = 2; 
+const EMPTY = 0;
 
 function change_game_display()
 {
@@ -40,7 +43,7 @@ function change_game_display()
     
 }
 
-function change_player_selection()
+function reset_player_selection()
 {
     var selection = document.getElementById("selection");
     for(line_number=1; line_number<=LINES; line_number++)
@@ -97,9 +100,48 @@ function change_game_content(selected_line, turn)
 
 function get_computer_selection()
 {
-    //minimax algorithm here 
-    //use temporary array for game_content
+    var test_game_content = [0,0,0,0,0,0,0,0,0,0];
+    game_content.forEach(function(item, index, array){
+        test_game_content[index] = item;
+    }) 
+
+    selection = minimax(test_game_content, DEPTH, ALPHA, BETA, COMPUTER_TURN);
     return selection
+}
+
+function minimax(test_game_content, depth, new_alpha, new_beta, turn)
+{
+    game_over = true;
+    for(i=0; i<10; i++)
+    {
+        if(test_game_content[i]==0)
+        {
+            game_over = false; 
+            break; 
+        }
+    }
+
+    if(game_over || depth==0)
+        return evaluate(test_game_content);
+
+    if(turn==COMPUTER_TURN)
+    {
+        var max_evaluation = -Infinity;
+        //maximizing player
+    }
+
+    if(turn==PLAYER_TURN)
+    {
+        var min_evaluation = Infinity;
+        //minimizing player
+    }
+
+}
+
+function evaluate(test_game_content)
+{
+    //???? how to evaluate 
+    return 0;
 }
 
 function game_changer()
@@ -109,12 +151,18 @@ function game_changer()
         var this_button       = document.getElementById("myButton");
         this_button.value     = "Next move"; 
         this_button.innerHTML = "Next move";
+        clear_selection_content(); 
+        reset_player_selection();
+        start = true; 
     }
-    var selected_line = get_player_selection();
-    change_game_content(selected_line, 2);
-    computer_selected_line = get_computer_selection(); 
-    change_game_content(computer_selected_line, 1);
-    change_game_display();
-    clear_selection_content(); 
-    change_player_selection(); 
+    else 
+    {
+        var selected_line = get_player_selection();
+        change_game_content(selected_line, 2);
+        computer_selected_line = get_computer_selection(); 
+        change_game_content(computer_selected_line, 1);
+        change_game_display();
+        clear_selection_content(); 
+        reset_player_selection(); 
+    }
 }
